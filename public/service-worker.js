ac22fcede67ae600811a19aa32fee75962757806
joubertzack-web <joubertzack@gmail.com>
2026-02-01 13:54:09 -0600

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,2 +1,5 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
+app.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "service-worker.js"));
+});
